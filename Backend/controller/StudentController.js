9fc2fa74ca3966ccbef93d97bbbb83ee89b9c3c2
Adedoyin13 <olajideadedoyinrodiyat@gmail.com>
2024-11-25 18:25:53 +0100

    Models

---
+++ Backend/controller/StudentController.js
@@ -0,0 +1,3 @@
+const studentModel = require('./../models/StudentModel');
+const generateToken = require('./../utils/index');
+const asyncHandler = require('express-async-handler');
---
+++ Backend/models/StudentModel.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose')
+
+const guardianSchema = new mongoose.Schema ({
+    guardianName: {
+        type: String,
+        required: true
+    },
+    guardianEmail: {
+        type: String,
+        required: [true, 'Please add an email'],
+        trim: true
+    }
+})
+
+
+const studentSchema = new mongoose.Schema ({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ['female', 'male', 'others']
+    },
+    nationality: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an email'],
+        unique: true,
+        trim: true
+    },
+    guardian: guardianSchema,
+    room: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Room',
+        default: null
+    },
+    role: {
+        type: String,
+        enum: ['student'],
+        default: 'student'
+    },
+    checkedIn: {
+        type: Boolean,
+        default: false
+    },
+    checkedInTime: {
+        type: Date,
+        default: null
+    },
+    checkedOutTime: {
+        type: Date,
+        default: null
+    }
+}, {timestamps: true, })
+
+const Student = mongoose.model('Student', studentSchema)
+model.exports = Student;
